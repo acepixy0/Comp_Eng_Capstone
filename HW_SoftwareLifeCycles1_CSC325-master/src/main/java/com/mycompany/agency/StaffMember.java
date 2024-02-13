7d6913993ff239c3d2b2e16d11f488d4900c1c6f
acepixy0 <hagood.jordan282@gmail.com>
2024-02-12 19:35:41 -0500

    To do 1 - 2

---
+++ HW_SoftwareLifeCycles1_CSC325-master/src/main/java/com/mycompany/agency/StaffMember.java
@@ -0,0 +1,43 @@
+package com.mycompany.agency;
+
+
+
+// TODO 1: Make this class work and public
+public class StaffMember
+{
+    protected String name;
+    protected String address;
+    protected String phone;
+
+    //-----------------------------------------------------------------
+    //  Constructor: Sets up this staff member using the specified
+    //  information.
+    //-----------------------------------------------------------------
+
+    public StaffMember(String eName, String eAddress, String ePhone)
+    {
+        name = eName;
+        address = eAddress;
+        phone = ePhone;
+    }
+
+    //-----------------------------------------------------------------
+    // TODO 2: Returns a string including the basic employee information.
+    //-----------------------------------------------------------------
+
+
+    @Override
+    public String toString() {
+        return "StaffMember{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    //-----------------------------------------------------------------
+    //  Derived classes must define the pay method for each type of
+    //  employee.
+    //-----------------------------------------------------------------
+    public abstract double pay();
+}
