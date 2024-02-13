5b514c0aa831224ce999ebed511d82c0324314d9
acepixy0 <hagood.jordan282@gmail.com>
2024-02-12 19:35:18 -0500

    To do 1 - 4

---
+++ HW_SoftwareLifeCycles1_CSC325-master/src/main/java/com/mycompany/agency/StaffEmployee.java
@@ -0,0 +1,44 @@
+package com.mycompany.agency;
+
+
+// TODO 1: Make StaffEmployee a child of StaffMember
+
+public class StaffEmployee extends StaffMember
+{
+    protected String socialSecurityNumber;
+    protected double payRate;
+
+    //-----------------------------------------------------------------
+    //  TODO 2: Sets up this staff employee with the specified
+    //  information.
+    //-----------------------------------------------------------------
+
+    public StaffEmployee(String eName, String eAddress, String ePhone, String socSecNumber, double rate) {
+        super(eName, eAddress, ePhone);
+        socialSecurityNumber = socSecNumber;
+        payRate = rate;
+    }
+
+    //-----------------------------------------------------------------
+    // TODO 3: complete the implementation of toString to return information
+    //          about a staff employee as a string.
+    //-----------------------------------------------------------------
+    @Override
+    public String toString() {
+        return "StaffEmployee{" +
+                "socialSecurityNumber='" + socialSecurityNumber + '\'' +
+                ", payRate=" + payRate +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    //-----------------------------------------------------------------
+    // TODO 4:  Returns the pay rate for this staff employee.
+    //-----------------------------------------------------------------
+    public double pay()
+    {
+        return this.payRate;
+    }
+}
